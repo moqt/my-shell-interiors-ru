287944ce9f19aaf173f3935f1923e60b8978798f
Ivan Kozlov <98733873+moqt@users.noreply.github.com>
2026-05-27 20:49:08 +0300

    Add files via upload

---
+++ assets/md-Cw6FkJ3i.js
@@ -0,0 +1,7 @@
+import{o as s,b as l,w as a,j as i,g as o,v as p,x as d,z as e}from"./modules/vue-2cqWfLG3.js";import{I as u}from"./slidev/default-C-yqSSS8.js";import{u as m,f}from"./slidev/context-Iku37eDu.js";import"./index-B9caWjkP.js";import"./modules/shiki-Bd1LD4Tj.js";const c="/ru/PhoneHor.png",C={__name:"slides.md__slidev_3",setup(g){const{$clicksContext:r,$frontmatter:n}=m();return r.setup(),(w,t)=>(s(),l(u,p(d(e(f)(e(n),2))),{default:a(()=>[i(` 
+# Early prototype / deployment
+
+The following video demonstrates furniture placement
+* This is a real-world project on a tight budget
+* The test allowed us to prove the concept with real data and validate performance on Android and iOS
+`),t[0]||(t[0]=o("h1",null,"Ранний прототип / деплой",-1)),t[1]||(t[1]=o("p",null,"Следующее видео демонстрирует расстановку мебели",-1)),t[2]||(t[2]=o("ul",null,[o("li",null,"Это проект по ремонту с ограниченным бюджетом"),o("li",null,"Тест позволил подтвердить концепцию на реальных данных и проверить производительность на Android и iOS")],-1)),t[3]||(t[3]=o("img",{src:c,class:"absolute bottom-5 left-1/2 -translate-x-1/2 w-150 -z-1"},null,-1))]),_:1},16))}};export{C as default};
