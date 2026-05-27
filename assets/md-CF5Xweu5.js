068e06c8697aa5783c693a552ee7938c42347c0a
Ivan Kozlov <98733873+moqt@users.noreply.github.com>
2026-05-27 21:33:52 +0300

    Add files via upload

---
+++ assets/md-CF5Xweu5.js
@@ -0,0 +1,7 @@
+import{o as s,b as l,w as a,j as i,g as e,v as p,x as u,z as o}from"./modules/vue-2cqWfLG3.js";import{I as d}from"./slidev/default-Dj2L4uMw.js";import{u as m,f}from"./slidev/context-BC5vHjyu.js";import"./index-46C4JdtK.js";import"./modules/shiki-Bd1LD4Tj.js";const c="/my-shell-interiors-ru/ru/PhoneHor.png",C={__name:"slides.md__slidev_3",setup(g){const{$clicksContext:r,$frontmatter:n}=m();return r.setup(),(w,t)=>(s(),l(d,p(u(o(f)(o(n),2))),{default:a(()=>[i(` 
+# Early prototype / deployment
+
+The following video demonstrates furniture placement
+* This is a real-world project on a tight budget
+* The test allowed us to prove the concept with real data and validate performance on Android and iOS
+`),t[0]||(t[0]=e("h1",null,"Ранний прототип / деплой",-1)),t[1]||(t[1]=e("p",null,"Следующее видео демонстрирует расстановку мебели",-1)),t[2]||(t[2]=e("ul",null,[e("li",null,"Это проект по ремонту с ограниченным бюджетом"),e("li",null,"Тест позволил подтвердить концепцию на реальных данных и проверить производительность на Android и iOS")],-1)),t[3]||(t[3]=e("img",{src:c,class:"absolute bottom-5 left-1/2 -translate-x-1/2 w-150 -z-1"},null,-1))]),_:1},16))}};export{C as default};
---
+++ assets/md-_Zn92H1V.js
@@ -0,0 +1,13 @@
+import{o as s,b as e,w as i,j as u,g as n,v as a,x as m,z as l}from"./modules/vue-2cqWfLG3.js";import{I as p}from"./slidev/default-Dj2L4uMw.js";import{u as d,f}from"./slidev/context-BC5vHjyu.js";import"./index-46C4JdtK.js";import"./modules/shiki-Bd1LD4Tj.js";const g="/my-shell-interiors-ru/money.webp",L={__name:"slides.md__slidev_9",setup(v){const{$clicksContext:o,$frontmatter:r}=d();return o.setup(),(w,t)=>(s(),e(p,a(m(l(f)(l(r),8))),{default:i(()=>[u(`
+# Monetization
+
+#### Freemium / subscriptions
+* Remove cooldown on LLM generation and ray tracing (subscription or one-time purchase)
+#### Advertising
+* Banners and interstitials, promoted products in search results
+#### Affiliate marketing
+* Bill of materials includes real purchasable products and contractor services
+#### Premium content
+* Ray-traced video walkthroughs of the designed space
+* Twinmotion-style interactive presentations
+`),t[0]||(t[0]=n("h1",null,"Монетизация",-1)),t[1]||(t[1]=n("h4",null,"Freemium / подписки",-1)),t[2]||(t[2]=n("ul",null,[n("li",null,"Снятие ограничений на LLM-генерацию и рейтрейсинг (подписка или единоразовый платёж)")],-1)),t[3]||(t[3]=n("h4",null,"Реклама",-1)),t[4]||(t[4]=n("ul",null,[n("li",null,"Баннеры и interstitial-объявления, продвижение товаров в поисковой выдаче")],-1)),t[5]||(t[5]=n("h4",null,"Партнёрский маркетинг",-1)),t[6]||(t[6]=n("ul",null,[n("li",null,"Ведомость материалов содержит реальные товары и услуги подрядчиков")],-1)),t[7]||(t[7]=n("h4",null,"Премиум-контент",-1)),t[8]||(t[8]=n("ul",null,[n("li",null,"Видео-облёты помещения с рейтрейсингом"),n("li",null,"Интерактивные презентации в стиле Twinmotion")],-1)),t[9]||(t[9]=n("img",{src:g,class:"absolute bottom--105 left-1/2 -translate-x-1/2 w-250 -z-1 opacity-30"},null,-1))]),_:1},16))}};export{L as default};
